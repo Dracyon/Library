7d43f9577609c5627da76dacaac8b065087b7c53
Dracyon <dracyon@gmail.com>
2016-05-02 00:09:49 +0200

    adding sorting and pagination

--- Library/Library/Controllers/BooksController.cs
+++ Library/Library/Controllers/BooksController.cs
@@ -1,10 +1,14 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 using Library.Repository.Models;
 using Library.Repository.Repository;
 using Library.Repository.Repository.IRepository;
+using PagedList;
 
 namespace Library.Controllers
 {
@@ -17,10 +21,80 @@ namespace Library.Controllers
 			_bookRepository = bookRepository;
 		}
         // GET: Books
-        public ActionResult Index()
+        public ActionResult Index(bool? isEdit, bool? isDeleted, bool? isRent, bool? isReturn, bool? isError, int? page, string sortOrder)
         {
-            var books = _bookRepository.GetBooks();
-            return View(books);
+	        try
+	        {
+		        int currentPage = page ?? 1;
+		        int pageSize = 20;
+				var books = _bookRepository.GetBooks();
+		        ViewBag.CurrentSort = sortOrder;
+		        ViewBag.Title = sortOrder == "Title" ? "TitleAsc" : "Title";
+		        ViewBag.Author = sortOrder == "Author" ? "AuthorAsc" : "Author";
+		        ViewBag.Isbn = sortOrder == "Isbn" ? "IsbnAsc" : "Isbn";
+				ViewBag.CreationDate = sortOrder == "CreationDate" ? "CreationDateAsc" : "CreationDate";
+				ViewBag.UpdateDate = sortOrder == "UpdateDate" ? "UpdateDateAsc" : "UpdateDate";
+
+		        switch (sortOrder)
+		        {
+					case "Title":
+				        books = books.OrderByDescending(b => b.Title).ToList();
+				        break;
+					case "TitleAsc":
+						books = books.OrderBy(b => b.Title).ToList();
+						break;
+					case "Author":
+						books = books.OrderByDescending(b => b.Author).ToList();
+						break;
+					case "AuthorAsc":
+						books = books.OrderBy(b => b.Author).ToList();
+						break;
+					case "Isbn":
+						books = books.OrderByDescending(b => b.Isbn).ToList();
+						break;
+					case "IsbnAsc":
+						books = books.OrderBy(b => b.Isbn).ToList();
+						break;
+					case "CreationDate":
+						books = books.OrderByDescending(b => b.CreationDate).ToList();
+						break;
+					case "CreationDateAsc":
+						books = books.OrderBy(b => b.CreationDate).ToList();
+						break;
+					case "UpdateDate":
+						books = books.OrderByDescending(b => b.UpdatenDate).ToList();
+						break;
+					case "UpdateDateAsc":
+						books = books.OrderBy(b => b.UpdatenDate).ToList();
+						break;
+					default:
+				        books = books?.OrderBy(b => b.Id).ToList();
+				        break;
+		        }
+
+		        if (isDeleted != null && isDeleted.Value)
+			        ViewBag.IsDeleted = true;
+		        if (isEdit != null && isEdit.Value)
+			        ViewBag.IsEdited = true;
+				if (isError != null && isError.Value)
+					ViewBag.IsError = true;
+		        if (isRent != null && isRent.Value)
+			        ViewBag.IsRent = true;
+		        if (isReturn != null && isReturn.Value)
+			        ViewBag.IsReturn = true;
+
+				var booksList = new List<Book>();
+
+		        if (books != null)
+			        booksList = books;
+
+		        return View(booksList.ToPagedList<Book>(currentPage, pageSize));
+	        }
+	        catch (Exception e)
+	        {
+				ViewBag.Error = true;
+	        }
+			return new HttpStatusCodeResult(HttpStatusCode.InternalServerError);
         }
 
         // GET: Books/Details/5
